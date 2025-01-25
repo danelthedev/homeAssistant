67697bb95e0ca3e8d4c8e4aee622648db6ba3ce3
Daniel Imbrea <danelthedev@gmail.com>
2025-01-25 16:33:59 +0200

    this time for sure

--- tts.py
+++ tts.py
@@ -11,11 +11,11 @@ def speak_text(text, language='en'):
     """
     # Create audio file
     tts = gTTS(text=text, lang=language)
-    tts.save("./tmp/speech.mp3")
+    tts.save("/tmp/speech.mp3")
 
     # Initialize pygame mixer
     pygame.mixer.init()
-    pygame.mixer.music.load("./tmp/speech.mp3")
+    pygame.mixer.music.load("/tmp/speech.mp3")
     pygame.mixer.music.play()
 
     # Wait for playback to finish
@@ -24,4 +24,4 @@ def speak_text(text, language='en'):
 
     # Clean up temporary file
     pygame.mixer.music.unload()
-    os.remove("./tmp/speech.mp3")
+    os.remove("/tmp/speech.mp3")
